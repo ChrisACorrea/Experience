ec3fd65957d858400c97735c52d7add8cb311062
ChrisACorrea <christopher.a.c@outlook.com>
2021-10-05 00:45:51 -0300

    Adicionar arquivos de projeto.

---
+++ desafio-01-numeros-primos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+Console.Write("Digite um número: ");
+int numeroDeEntrada = int.Parse(Console.ReadLine());
+bool ehPrimo = ehNumeroPrimo(numeroDeEntrada);
+
+Console.WriteLine($"O número {numeroDeEntrada}{(!ehPrimo ? " não": "")} é primo");
+Console.ReadKey();
+
+bool ehNumeroPrimo(int numero)
+{
+	int totalDeDivisoesExatas = 0;
+
+	for (int i = 1; i <= numero; i++)
+	{
+		double restoDaDivisao = numero % i;
+
+		if (restoDaDivisao is 0)
+		{
+			totalDeDivisoesExatas++;
+		}
+
+		if (totalDeDivisoesExatas is > 2)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
