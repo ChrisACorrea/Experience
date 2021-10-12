cfa894da894916ffacc5c3c911474d9ed309bd15
ChrisACorrea <christopher.a.c@outlook.com>
2021-10-12 16:12:48 -0300

    Melhorias no desafio da pirâmide

--- desafio-03-piramides/Program.cs
+++ desafio-03-piramides/Program.cs
@@ -1,10 +1,55 @@
 ﻿using System;
 
-Console.Write("Escolha a altura da pirâmide: ");
-int altura = int.Parse(Console.ReadLine());
+const int alturaMinima = 3;
+const int alturaMaxima = 8;
+bool valorValido = false;
+int altura = 0;
+
+while (!valorValido)
+{
+	Console.Write($"Escolha uma altura para a pirâmide entre {alturaMinima} e {alturaMaxima}: ");
+
+	try
+	{ 
+		altura = int.Parse(Console.ReadLine());
+
+		if (altura is < alturaMinima or > alturaMaxima)
+		{
+			Console.WriteLine("Valor não permitido.");
+			Console.WriteLine();
+			continue;
+		}
+
+		valorValido = true;
+		Console.Clear();
+	}
+	catch (FormatException)
+	{
+		Console.WriteLine("O valor inserido deve ser um número e um número inteiro. Também não pode ser vazio.");
+	}
+	catch (OverflowException)
+	{
+		Console.WriteLine("Ops!!! Esse é um valor muito alto. Tente com um menor.");
+	}
+	catch (Exception)
+	{
+		Console.WriteLine("Deu ruim!!!");
+		throw;
+	}
+	finally
+	{
+		Console.WriteLine();
+	}
+}
+
+Console.WriteLine($"Altura escolhida: {altura}.");
 Console.WriteLine();
 
 imprimePiramides(altura);
+
+Console.WriteLine();
+Console.WriteLine("Pressione qualquer tecla para encerrar.");
+Console.ReadKey();
 
 void imprimePiramides(int altura)
 {
